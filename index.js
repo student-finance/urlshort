eb143a12a3ec845f9cd81e375c956839af8bfaa1
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-05-19 18:30:31 +0530

    added different workarounds

---
+++ index.js
@@ -0,0 +1,46 @@
+addEventListener('fetch', event => {
+  try {
+    if (request.method.toUpperCase() === 'POST') return event.respondWith(handlePostRequest(event.request));
+    if (request.method.toUpperCase() === 'GET') return event.respondWith(handleGETRequest(event.request));
+    return event.respondWith(fetch(event.request));
+  } catch (e) {
+    return event.respondWith(new Response('Error thrown ' + e.message));
+  }
+});
+
+function return404(msg = "Nothing here :)"){
+  return new Response(
+    `<!doctype html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+      <h1 >`+
+      msg+
+      `</h1>
+    </body>
+    </html>`,
+      {
+        status: 404,
+        headers: { 'content-type': 'text/html' },
+      },
+    )
+};
+
+async function handleGETRequest(request) {
+  const url = new URL(request.url);
+
+  console.log(request.headers.get('user-agent'), new Date().toLocaleDateString(), url.pathname.substring(1));
+  var split_url = url.pathname.split('/')
+  if (split_url.length < 2 || split_url.length > 3 || split_url[1] == "favicon.ico" || split_url[1] == "") {
+    return return404();
+  }
+  let path = split_url[1];
+  const redirect_path = await URL_SHORT.get(path);
+  if (redirect_path === null) {
+    return return404();
+  }
+  return Response.redirect(redirect_path, 308);
+}
